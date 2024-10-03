68510a278dd4a6ee069a5b0da14ad9e2b4172eb1
Curs3W4ll <corentin.hervaud@epitech.eu>
2024-10-03 23:01:53 +0200

    Add base project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="buttons">
+    <button class="button is-skeleton">Button</button>
+    <button class="button is-link is-skeleton">Link</button>
+    <button class="button is-primary is-skeleton">Primary</button>
+    <button class="button is-success is-skeleton">Success</button>
+    <button class="button is-info is-skeleton">Info</button>
+    <button class="button is-warning is-skeleton">Warning</button>
+    <button class="button is-danger is-skeleton">Danger</button>
+  </div>
+  <input class="input is-skeleton" placeholder="This is a placeholder" />
+</template>
